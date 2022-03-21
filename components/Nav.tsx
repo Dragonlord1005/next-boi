e9c4367dd59530d7f2581a94370252a2129733e7
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-21 17:36:23 +0000

    prefetching is deprecated

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -28,7 +28,7 @@ export default function Nav() {
           </Typography>
           <br />
           <Typography component="div" sx={{ mr: 1.5 }}>
-            <Link prefetch={true} href="/about">About</Link>
+            <Link href="/about">About</Link>
           </Typography>
         </Toolbar>
       </AppBar>
