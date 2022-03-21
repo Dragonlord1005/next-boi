d1237614f80afdce47e7ac2abdb07b09ad6d6773
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-21 11:21:37 -0600

    feat: Added material ui (#5)
    * added the neccesary packages
    
    * chore: added prettier
    
    * Formatted and added components
    
    * fix(deps): pin dependencies
    
    * chore(deps): update dependency eslint to v8.11.0
    
    * fix(deps): update dependency @mui/material to v5.5.1
    
    * Why am I doing this
    
    * fix(deps): pin dependency @mui/icons-material to 5.5.1
    
    * I DID IT
    
    * possible fix for a compilation issue
    
    * hopefully fixed the compilation issue
    
    * fix: Might have fixed the compilation issue
    
    * chore(deps): update dependency prettier to v2.6.0
    
    * started adding dark mode
    
    * formatted files
    
    * no dark mode right now
    
    * chore(deps): update dependency @types/react to v17.0.41
    
    * Removed now useless buttons.
    
    * adjusted the spacing of buttons
    
    * more stuff
    
    * deduplicated and edited prettierignore
    
    * removed a useless button for now, not sure if it will return
    
    * pretty much finished
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

---
+++ components/Nav.tsx
@@ -0,0 +1,37 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+{
+  /*import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";*/
+}
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/*<IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+          >
+            <MenuIcon />
+          </IconButton>*/}
+          <Typography component="div" sx={{ mr: 1.5 }}>
+            <Link href="/">Home</Link>
+          </Typography>
+          <br />
+          <Typography component="div" sx={{ mr: 1.5 }}>
+            <Link href="/about">About</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import '@fontsource/roboto/400.css';
-import Button from '@mui/material/Button';
-import Link from 'next/link';
+import type { NextPage } from "next";
+import * as React from "react";
+import Head from "next/head";
+// import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import "@fontsource/roboto/400.css";
+// import Button from "@mui/material/Button";
+// import Link from "next/link";
+import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
   return (
@@ -15,33 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Head>
+        <Nav />
+      </Head>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to my Blog
-        </h1>
-        <br />
-
-        <Button variant="contained">
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </Button>
+        <h1 className={styles.title}>Welcome to my Blog</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
