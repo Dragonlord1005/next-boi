11ee372333bfb159dcec291491fed1719e2eb6c4
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-22 16:33:51 +0000

    fixed a grammatical error

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,7 +22,7 @@ const About: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>About</h1>
         <a className={styles.description}>
-          This is a prject where my goal is to make a blog using only typescript
+          This is a project where my goal is to make a blog using only typescript
           and Next.js
         </a>
         <a className={styles.text}>
