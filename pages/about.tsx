aca5adfcba0c298f9c34ed0a5386b4cad6f24c16
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-22 16:08:11 +0000

    Added more text

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,7 +22,7 @@ const About: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>About</h1>
         <a className={styles.description}>This is a prject where my goal is to make a blog using only typescript and Next.js</a>
-        <a className={styles.text}>So far its been quite a lot of work to make stuff look the way I want it to</a>
+        <a className={styles.text}>So far its been quite a lot of work to make stuff look the way I want it to, but fun at the same time</a>
       </main>
     </div>
   );
