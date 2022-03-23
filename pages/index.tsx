2def0e85076331de119980728e545176ef36095c
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-23 19:01:50 +0000

    removed useless comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import type { NextPage } from "next";
 import type { ReactElement } from "react";
-//import * as React from "react";
 import Head from "next/head";
-// import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import "@fontsource/roboto/400.css";
-// import Button from "@mui/material/Button";
-// import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
