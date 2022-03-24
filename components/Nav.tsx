99815f4220864870798a35ddb2afba1c4c580b35
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-24 19:32:21 +0000

    Started adding prefetching

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,8 +8,10 @@ import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";*/
 }
 import Link from "next/link";
+import { useRouter } from 'next/router'
 
 export default function Nav() {
+  const router = useRouter()
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,11 +26,29 @@ export default function Nav() {
             <MenuIcon />
           </IconButton>*/}
           <Typography component="div" sx={{ mr: 1.5 }}>
-            <Link href="/">Home</Link>
+            <Link href="/" prefetch={false}>
+              <a
+                onMouseEnter={() => {
+            router.prefetch('/about')
+            console.log('prefetching /about!')
+                }}
+              >
+                Home
+              </a>
+            </Link>
           </Typography>
           <br />
           <Typography component="div" sx={{ mr: 1.5 }}>
-            <Link href="/about">About</Link>
+            <Link href='/about' prefetch={false}>
+              <a
+                onMouseEnter={() => {
+            router.prefetch('/about')
+            console.log('prefetching /about!')
+                }}
+              >
+                About
+              </a>
+            </Link>
           </Typography>
           <br />
           <Typography component="div" sx={{ mr: 1.5 }}>
