3b00563f28e604abe41461828d08140a9a61fe1f
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-24 16:22:22 +0000

    added a template page, for a baseline

---
+++ pages/posts/template.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "./blog.module.css";
+import "@fontsource/roboto/400.css";
+
+const Template: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TEMPLATE</title>
+        <meta name="description" content="TEMPLATE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>TEMPLATE</h1>
+        <h2 className={styles.description}>PLACEHOLDER</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Template;
