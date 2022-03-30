ad3c6ce4426d2503409ad3ddd27776ceb47a2f07
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-30 19:46:40 +0000

    it finally works

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,4 @@
 import React, { ReactNode } from "react";
-import Link from "next/link";
-import Head from "next/head";
-import Nav from "./Nav";
 import Navbar from "./Navbar/Navbar";
 
 type Props = {
@@ -11,9 +8,7 @@ type Props = {
 const links = [{ link: '/', label: 'Home' }]
 const Layout = ({ children }: Props) => (
   <div>
-    <Head>
-      <Navbar links={links}  />
-    </Head>
+    <Navbar links={links}  />
     {children}
   </div>
 );
