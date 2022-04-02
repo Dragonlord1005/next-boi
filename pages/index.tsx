97db90c6ffd3bdff28d8c2ed62e2f903a94c4b3f
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-02 20:37:04 +0000

    made big font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from "next";
-import type { ReactElement } from "react";
+//import type { ReactElement } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Title } from "@mantine/core";
+import { createStyles, Title } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+
+  title: {
+    //color: theme.white,
+    fontSize: 100,
+    fontWeight: 900,
+    lineHeight: 1.1,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 40,
+      lineHeight: 1.2,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 28,
+      lineHeight: 1.3,
+    },
+  },
+}));
+                               
 
 const Home: NextPage = () => {
+  const { classes } = useStyles();
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +36,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Title align="center">Welcome to my Blog</Title>
+        <Title align="center" className={classes.title}>Welcome to my Blog</Title>
       </main>
     </div>
   );
