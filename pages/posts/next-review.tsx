61e04d837a1fdd01765a168172e3e012d8829717
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-02 18:32:33 -0600

    feat(blog): Added more content to the upcoming review

--- pages/posts/next-review.tsx
+++ pages/posts/next-review.tsx
@@ -1,20 +1,73 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "./blog.module.css";
+import { createStyles, Title, Text, Container } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    //color: theme.white,
+    fontSize: 75,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    paddingBottom: 14,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 40,
+      lineHeight: 1.2,
+    },
+
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: 28,
+      lineHeight: 1.3,
+    },
+  },
+
+  description: {
+    color: theme.white,
+    maxWidth: 600,
+
+    [theme.fn.smallerThan("sm")]: {
+      maxWidth: "100%",
+      fontSize: theme.fontSizes.sm,
+    },
+  },
+
+  container: {
+    height: 700,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    paddingBottom: theme.spacing.xl * 6,
+    zIndex: 1,
+    position: "relative",
+
+    [theme.fn.smallerThan("sm")]: {
+      height: 500,
+      paddingBottom: theme.spacing.xl * 3,
+    },
+  },
+}));
 
 const NextReview: NextPage = () => {
+  const { classes } = useStyles();
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next.js Review</title>
-        <meta name="description" content="From a beginners perspective" />
+        <title>Next Review</title>
+        <meta name="description" content="Next Review" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>My Next.js review</h1>
-        <h2 className={styles.description}>4/1/22</h2>
-      </main>
+      <Container className={styles.main}>
+        <Title align="center" className={classes.title}>
+          Next Review
+        </Title>
+        <Text>1/14/22</Text>
+        <article>
+          <Text>PLACEHOLDER</Text>
+        </article>
+      </Container>
     </div>
   );
 };
