f10129d0f2be2e329c648fa4d70d99bf30e800bf
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-03 18:20:57 -0600

    chore(blog): Added the blog file for mantine

---
+++ pages/posts/mantine-review.tsx
@@ -0,0 +1,81 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "./blog.module.css";
+import { createStyles, Title, Text, Container } from "@mantine/core";
+const useStyles = createStyles((theme) => ({
+    title: {
+        //color: theme.white,
+        fontSize: 75,
+        fontWeight: 900,
+        lineHeight: 1.1,
+        paddingBottom: 14,
+
+        [theme.fn.smallerThan("sm")]: {
+            fontSize: 40,
+            lineHeight: 1.2,
+        },
+
+        [theme.fn.smallerThan("xs")]: {
+            fontSize: 28,
+            lineHeight: 1.3,
+        },
+    },
+
+    description: {
+        color: theme.white,
+        maxWidth: 600,
+
+        [theme.fn.smallerThan("sm")]: {
+            maxWidth: "100%",
+            fontSize: theme.fontSizes.sm,
+        },
+    },
+
+    container: {
+        height: 700,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "flex-start",
+        paddingBottom: theme.spacing.xl * 6,
+        zIndex: 1,
+        position: "relative",
+
+        [theme.fn.smallerThan("sm")]: {
+            height: 500,
+            paddingBottom: theme.spacing.xl * 3,
+        },
+    },
+}));
+
+const MantineReview: NextPage = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { classes } = useStyles();
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Mantine Review</title>
+                <meta name="My mantine review" content="Mantine Review" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Container className={styles.main}>
+                <Title align="center" className={classes.title}>
+                    Mantine Review
+                </Title>
+                <Text className={classes.description}>Placeholder</Text>
+                <article>
+                    <Text>hi</Text>
+                </article>
+            </Container>
+        </div>
+    );
+};
+
+export default MantineReview;
+
+
+
+
+
+
