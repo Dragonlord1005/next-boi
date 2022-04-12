9f12c8e8d59110145bb99ed844774e426e84fa8e
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-12 15:03:24 +0000

    Began adding in storybook support for lightdark button

---
+++ components/LightDark/LightDark.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import LightDark from "./LightDark";
+
+export default {
+  title: "LightDark",
+  component: LightDark,
+} as ComponentMeta<typeof LightDark>;
