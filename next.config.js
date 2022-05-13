d9f50aabe363c75e793854bc9f7362820bf796ee
Dragonlord1005 <rjameslangston@icloud.com>
2022-05-13 13:57:42 -0600

    Formatted a single file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['react-daisyui']) // or whatever library giving trouble
+const withTM = require("next-transpile-modules")(["react-daisyui"]); // or whatever library giving trouble
 
 module.exports = withTM({
   reactStrictMode: true,
-})
+});
