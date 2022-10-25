410881f2e3ea3e558ce8a03bc617ef1cca06c43b
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-25 23:03:50 +0000

    idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,9 @@ const Home: NextPage = () => {
         <title>My Blog</title>
         <meta name="description" content="A personal blog" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image"
+          content="https://next-boi-git-og-image-dragonlord1005.vercel.app/api/og"
+        />
       </Head>
       <main className={styles.main}>
         <Title align="center" className={classes.title}>
