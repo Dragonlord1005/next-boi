3b92f22f6c400ae3ad252aba2a1a604bfb3b4f66
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-01-05 23:38:49 +0000

    forgot a comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import localFont from '@next/font/local'
+// import localFont from '@next/font/local'
 
 // const mona = localFont({ src: './fonts/Mona-Sans.woff2', variable: '--font-mona', weight: '200 900'});
 // const hubot = localFont({ src: './fonts/Hubot-Sans.woff2', variable: '--font-hubot', weight: '200 900'});
